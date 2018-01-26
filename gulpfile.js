7ef054e033f473c72dd2c94db6133ff35cce4e5f
Mike Perna <mikeaperna@gmail.com>
2018-01-26 15:13:55 -0500

    zurb foundation 6; update gulp process and added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var notify = require('gulp-notify');
 
 gulp.task('sass', function () {
   return gulp.src('./assets/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+  	.pipe(sass({
+  		includePaths: ['./node_modules/foundation-sites/scss']
+	}).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 9', 'Android >= 2.3', 'ios >= 7']
+    }))
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(notify('Sass task complete!'));
 });
 
 gulp.task('watch', function () {
